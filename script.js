28aeb4b1a304f2c58c043a10dd77c804b204c8c3
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-03-03 13:39:01 +0530

    Completed questions

--- script.js
+++ script.js
@@ -1,3 +1,126 @@
+// 12. Write an ES6 function that takes an array of objects containing employee information (name, age, salary) and returns an array with only the employees who earn a salary above 22000 and age is above 25.
+
+const filterBySalary = (employees, salary, age) =>
+  employees.filter(
+    (employee) => employee.salary > salary && employee.age > age && employee
+  );
+
+const employees = [
+  { name: "Abhay", age: 25, salary: 20000 },
+  { name: "Joice", age: 30, salary: 35000 },
+  { name: "Reena", age: 35, salary: 15000 },
+  { name: "Jeena", age: 40, salary: 50000 },
+];
+
+const filteredEmployees = filterBySalary(employees, 22000, 25);
+console.log(filteredEmployees);
+// Output: [{name: "Joice", age: 30, salary: 35000}, {name: "Jeena", age: 40, salary: 50000}]
+
+/* 
+// 11. Write an ES6 function that takes an array of objects containing book information (title, author, genre) and returns an array with only the books of "Science Fiction".
+
+// const filterByGenre = (books, genre) =>
+//   books.filter((book) => book.genre === genre);
+
+const filterByGenre = (books, genre) =>
+  books.filter(findBookByGenre.bind(genre));
+
+const findBookByGenre = function (book) {
+  // console.log(this);
+  return String(this) === book.genre;
+};
+
+const books = [
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    genre: "Science Fiction",
+  },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction" },
+  { title: "1984", author: "George Orwell", genre: "Science Fiction" },
+  { title: "Pride and Prejudice", author: "Jane Austen", genre: "Romance" },
+];
+
+const filteredBooks = filterByGenre(books, "Science Fiction");
+console.log(filteredBooks);
+// Output: [{title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction"}, {title: "1984", author: "George Orwell", genre: "Science Fiction"}]
+ */
+
+/* 
+// 10. Write an ES6 function that takes an array of objects with the properties name, age and city, and returns an array with only the objects that have a city property of "Indore" and age greater than 70.
+
+const filterByCityAndAge = (people) =>
+  people.filter(
+    (person) =>
+      person.city.toUpperCase() === "INDORE" && person.age > 70 && person
+  );
+
+const people = [
+  { name: "Ridhima", age: 75, city: "Indore" },
+  { name: "Akshay", age: 60, city: "Delhi" },
+  { name: "Udit", age: 80, city: "Indore" },
+  { name: "Venki", age: 80, city: "Bangalore" },
+];
+const filteredPeople = filterByCityAndAge(people);
+console.log(filteredPeople);
+// Output: [{ name: 'Ridhima', age: 75, city: 'Indore' }, { name: 'Udit', age: 80, city: 'Indore' }]
+*/
+
+/* 
+// 9. Write an ES6 function that takes an array of objects with the properties name and score, and returns an array with only the objects that have a score greater than 80.
+
+const filterByScore = (students) =>
+  students.filter((student) => student.score > 80 && student);
+
+const students = [
+  { name: "Aditya", score: 85 },
+  { name: "Bob", score: 75 },
+  { name: "Charlie", score: 90 },
+];
+
+console.log(filterByScore(students));
+// Output: [{ name: 'Aditya', score: 85 }, { name: 'Charlie', score: 90 }]
+*/
+
+/* 
+// 8. Write an ES6 function that takes an array of objects with the properties name and age, and returns an array with only the objects that have an age greater than 30.
+
+const filterByAge = (people) =>
+  people.filter((person) => person.age > 30 && person);
+
+const people = [
+  { name: "Rahul", age: 25 },
+  { name: "Raj", age: 35 },
+  { name: "Vijay", age: 45 },
+];
+
+console.log(filterByAge(people));
+// Output: [{ name: 'Raj', age: 35 }, { name: 'Vijay', age: 45 }]
+*/
+
+/* 
+// 7. Write an ES6 function that takes an array of numbers and returns an array with only the numbers that are divisible by 3 and also by 5.
+
+const isDivisibleBy3and5 = (numsArr) =>
+  numsArr.filter((num) => num % 3 === 0 && num % 5 === 0);
+
+const numbers = [1, 2, 30, 4, 15];
+console.log(isDivisibleBy3and5(numbers));
+// Output: [30, 15]
+*/
+
+/* 
+// 6. Write an ES6 function that takes an array of strings and returns an array with only the strings that have a length greater than 5.
+
+const filterLongStrings = (fruits) =>
+  fruits.filter((fruit) => fruit.length > 5);
+
+const fruits = ["apple", "banana", "cherry", "watermelon", "pineapple"];
+console.log(filterLongStrings(fruits));
+// Output: ["banana", "cherry", "watermelon", "pineapple"]
+ */
+
+/* 
 // 5. Write an ES6 function that takes an array of objects representing cities and returns an array of their names using the map method.
 
 const getCityNames = (cities) => cities.map(({ name }) => name);
@@ -10,6 +133,7 @@ const cities = [
 
 console.log(getCityNames(cities));
 // Output: ["New York", "Los Angeles", "Bangalore"]
+*/
 
 /* 
 // 4. Write an ES6 function that takes an array of objects representing products and returns an array with the product names in all lower case letters using the map method.
